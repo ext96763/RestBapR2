bb1daf8a5c68177f490a4f7d8a89c0cbb18c85b0
ext96763 <sergej.horcicka@gmail.com>
2017-04-23 13:40:23 +0200

    SHO - mybatis configuration, swagger

---
+++ src/main/java/eu/profinit/opendata/utils/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package eu.profinit.opendata.utils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+/**
+ * Created by User on 22.04.2017.
+ */
+
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket newsApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Opendata, veřejné zakázky")
+                .description("Rest API Powered By Profinit.eu")
+                .termsOfServiceUrl("http://www-03.ibm.com/software/sla/sladb.nsf/sla/bm?Open")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://github.com/IBM-Bluemix/news-aggregator/blob/master/LICENSE")
+                .version("1.0")
+                .build();
+    }
+}
